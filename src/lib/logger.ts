9d133a0adecf1e638b123a732582226e399291b7
hakim-asrori <saneglos005@gmail.com>
2025-06-26 16:51:16 +0700

    added condition logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -24,21 +24,23 @@ const logger = winston.createLogger({
 export default logger;
 
 export async function createLogger() {
-  const headerList = await headers();
+  if (process.env.NODE_ENV === "development") {
+    const headerList = await headers();
 
-  const ip = headerList.get("x-forwarded-for")?.split(",")[0] || "unknown";
+    const ip = headerList.get("x-forwarded-for")?.split(",")[0] || "unknown";
 
-  const method = headerList.get("x-method") || "get";
-  const url = headerList.get("referer") || "unknown";
+    const method = headerList.get("x-method") || "get";
+    const url = headerList.get("referer") || "unknown";
 
-  return {
-    info: (msg: string) =>
-      logger.info(`[${ip}] [${method}] [${url}] - REQ ${msg}`),
-    error: (msg: string) =>
-      logger.error(`[${ip}] [${method}] [${url}] - RES ${msg}`),
-    api: (msg: string, isError = false) =>
-      isError
-        ? logger.error(`[${ip}] - HIT API ${msg}`)
-        : logger.info(`[${ip}] - HIT API ${msg}`),
-  };
+    return {
+      info: (msg: string) =>
+        logger.info(`[${ip}] [${method}] [${url}] - REQ ${msg}`),
+      error: (msg: string) =>
+        logger.error(`[${ip}] [${method}] [${url}] - RES ${msg}`),
+      api: (msg: string, isError = false) =>
+        isError
+          ? logger.error(`[${ip}] - HIT API ${msg}`)
+          : logger.info(`[${ip}] - HIT API ${msg}`),
+    };
+  }
 }
